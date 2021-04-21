80f11c861b6273dc91821c43e3bc6e0aebb2d4a7
Ziyobek <khazratkulov.zt@phystech.edu>
2021-04-21 16:46:25 +0300

    removed print_menu

--- main.py
+++ main.py
@@ -4,14 +4,7 @@
 
 
 
-# Print menu
-def print_menu(menu_options):
-    option_keys = list(menu_options.keys())
 
-    print("Here are your options:")
-    print("------")
-    for key in option_keys:
-        print(key + ":\t" + menu_options[key]["text"])
 
 
 # Play with our toys
