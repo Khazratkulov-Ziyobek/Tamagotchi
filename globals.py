bf0bf69a8238d9c87b8d3e2494bdd73e57ed0a70
Ziyobek <khazratkulov.zt@phystech.edu>
2021-04-21 18:17:35 +0300

    added some changes

--- globals.py
+++ globals.py
@@ -1,6 +1,8 @@
 # pet dictionary
+global pet
 pet = {"name": "", "type": "", "age": 0, "hunger": 0, "toys": []}
 # pet toys data object
+global cat_toys, dog_toys, fish_toys, pet_toys
 cat_toys = ["scratching post", "toy mouse", "ball of yarn"]
 dog_toys = ["chew boy", "stick", "frisbee"]
 fish_toys = ["undersea castle", "fake coral", "buried treasure"]
