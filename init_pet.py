1c933a4d58fd92a97b88e9545e56ac65f190ec6e
Ziyobek <khazratkulov.zt@phystech.edu>
2021-04-21 16:28:54 +0300

    Added function init_pet

---
+++ init_pet.py
@@ -0,0 +1,17 @@
+# Prompt for different options of pet type
+def init_pet():
+    # get the input of what type pet this is
+    pet_type = ""
+
+    pet_options = list(pet_toys.keys())
+
+    # validate the input
+    while pet_type not in pet_options:
+        print("Please input a type of pet from the following options: ")
+        for option in pet_options:
+            print(option)
+        pet_type = input("Please input one of the pets: ")
+    # write in the pet type into the database
+    pet["type"] = pet_type
+    # name our pet
+    pet["name"] = input("What would you like to name your " + pet["type"] + "? ")
