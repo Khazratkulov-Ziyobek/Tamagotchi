ae66b471faa748ef0d6d154723f495e4ac0f004f
Ziyobek <khazratkulov.zt@phystech.edu>
2021-04-21 18:27:30 +0300

    some changes in feed_pet.py

--- feed_pet.py
+++ feed_pet.py
@@ -1,8 +1,11 @@
+import globals
+
+
 # Feed our pet
 def feed_pet():
     # handle negative edge cases
-    new_hunger = pet["hunger"] - 10
+    new_hunger = globals.pet["hunger"] - 10
     if new_hunger < 0:
         new_hunger = 0
-    pet["hunger"] -= new_hunger
+    globals.pet["hunger"] -= new_hunger
     print("Fed your pet, decreasing hunger by 10")
