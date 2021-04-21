441aae8e11a62bcc1ede6922da6e9b6a7444698e
Ziyobek <khazratkulov.zt@phystech.edu>
2021-04-21 16:35:24 +0300

    Added variables

---
+++ globals.py
@@ -0,0 +1,7 @@
+# pet dictionary
+pet = {"name": "", "type": "", "age": 0, "hunger": 0, "toys": []}
+# pet toys data object
+cat_toys = ["scratching post", "toy mouse", "ball of yarn"]
+dog_toys = ["chew boy", "stick", "frisbee"]
+fish_toys = ["undersea castle", "fake coral", "buried treasure"]
+pet_toys = {"cat": cat_toys, "dog": dog_toys, "fish": fish_toys}
