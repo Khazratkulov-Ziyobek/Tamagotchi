6b831107e2b3cec5490c2f07e93afe4cfb220630
Ziyobek <khazratkulov.zt@phystech.edu>
2021-04-21 16:30:53 +0300

    removed function init_pet

--- main.py
+++ main.py
@@ -7,23 +7,7 @@ fish_toys = ["undersea castle", "fake coral", "buried treasure"]
 pet_toys = {"cat": cat_toys, "dog": dog_toys, "fish": fish_toys}
 
 
-# Prompt for different options of pet type
-def init_pet():
-    # get the input of what type pet this is
-    pet_type = ""
 
-    pet_options = list(pet_toys.keys())
-
-    # validate the input
-    while pet_type not in pet_options:
-        print("Please input a type of pet from the following options: ")
-        for option in pet_options:
-            print(option)
-        pet_type = input("Please input one of the pets: ")
-    # write in the pet type into the database
-    pet["type"] = pet_type
-    # name our pet
-    pet["name"] = input("What would you like to name your " + pet["type"] + "? ")
 
 
 # Print menu
