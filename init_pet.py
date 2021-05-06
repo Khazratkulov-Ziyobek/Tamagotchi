cb6ef9853a3621e5305f39700fffd8729d48d095
Ziyobek <khazratkulov.zt@phystech.edu>
2021-05-06 17:12:50 +0300

    changed init_pet.py

--- init_pet.py
+++ init_pet.py
@@ -1,4 +1,4 @@
-import globals
+from globals import Globals
 
 
 # Prompt for different options of pet type
@@ -6,7 +6,7 @@ def init_pet():
     # get the input of what type pet this is
     pet_type = ""
 
-    pet_options = list(globals.pet_toys.keys())
+    pet_options = list(pet_toys.keys())
 
     # validate the input
     while pet_type not in pet_options:
@@ -15,6 +15,6 @@ def init_pet():
             print(option)
         pet_type = input("Please input one of the pets: ")
     # write in the pet type into the database
-    globals.pet["type"] = pet_type
+    pet["type"] = pet_type
     # name our pet
-    globals.pet["name"] = input("What would you like to name your " + globals.pet["type"] + "? ")
+    pet["name"] = input("What would you like to name your " + pet["type"] + "? ")
