29faf86b5d310b78889ee26930cd4a1374b02b0a
Ziyobek <khazratkulov.zt@phystech.edu>
2021-05-06 18:54:02 +0300

    modified print_stats.py

--- print_stats.py
+++ print_stats.py
@@ -3,12 +3,16 @@ from globals import Globals
 
 # print out stats about the current status of the pet
 def print_stats():
-    print("Your " + pet["type"] + " " + pet["name"] + " is doing great!")
-    if len(pet["toys"]) == 0:
+    # print("Your " + pet["type"] + " " + pet["name"] + " is doing great!")
+    print(f"Your {Globals.pet['type']} {Globals.pet['name']} is doing great!")
+    if len(Globals.pet["toys"]) == 0:
         print("Your pet currently has not toys")
     else:
-        print("Your pet currently has: " + str(len(pet["toys"])) + " toys, which are: ")
-    for toy in pet["toys"]:
+        # print("Your pet currently has: " + str(len(pet["toys"])) + " toys, which are: ")
+        print(f"Your pet currently has: {str(len(Globals.pet['toys']))} toys, which are: ")
+    for toy in Globals.pet["toys"]:
         print(toy)
-    print("Your pet is currently at hunger of " + str(pet["hunger"]) + " of a max of 100.")
-    print("Your pet is " + str(pet["age"]) + " days old.")
+    # print("Your pet is currently at hunger of " + str(pet["hunger"]) + " of a max of 100.")
+    print(f"Your pet is currently at hunger of {str(Globals.pet['hunger'])} of a max of 100.")
+    # print("Your pet is " + str(pet["age"]) + " days old.")
+    print(f"Your pet is {str(Globals.pet['age'])} days old.")
